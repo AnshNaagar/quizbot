f165d5fea3c760ebd6605a92a69544f4c9ecd347
AnshNaagar <158273761+AnshNaagar@users.noreply.github.com>
2025-09-27 18:07:51 +0530

    Update user_handlers.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,4 +1,5 @@
-from telegram import Update, ContextTypes
+from telegram import Update
+from telegram.ext import ContextTypes # ContextTypes ab telegram.ext se import hoga
 from database.db_manager import db_manager
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
